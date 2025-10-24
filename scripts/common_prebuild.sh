196c088e19683e66ef98692b22e496a7d09a2d66
Jeremy J. Peper <1000001101000>
2025-10-24 12:32:45 -0500

    various updates for 2025.08.1 support

--- scripts/common_prebuild.sh
+++ scripts/common_prebuild.sh
@@ -2,15 +2,26 @@
 
 [ -f ".firstrun" ] && exit 0
 
-
 ##patch to add sysbench and dependency may need to look into why never merged
 wget -nc "https://marc.info/?l=buildroot&m=170021431900448&q=raw" -O "$CONFIG_DIR/../patches/buildroot_add_sysbench.patch" 2>/dev/null
 wget -nc "https://marc.info/?l=buildroot&m=170021430500441&q=raw" -O "$CONFIG_DIR/../patches/buildroot_add_ck.patch" 2>/dev/null
 
-##patches I'm working on getting into buildroot project, should be safe even if build not using that app.
-for x in buildroot_zfs_initscripts buildroot_syslinux_gptmbr buildroot_mdadm_systemd buildroot_atd_dir buildroot_atd_systemd buildroot_add_sysbench buildroot_add_ck
+
+##merged at some point
+##fix for linux-tools dependency
+#wget -nc "https://marc.info/?l=buildroot&m=174765172328890&q=raw" -O "$CONFIG_DIR/../patches/buildroot_linux-tools_dep.patch" 2>/dev/null
+
+##bump zfs version that works with default kernel ver
+wget -nc "https://gitlab.com/buildroot.org/buildroot/-/commit/98399d11ff6afe053a052f867bb0610eaacd52a0.patch" -O "$CONFIG_DIR/../patches/buildroot_zfs_2.3.4.patch" 2>/dev/null
+
+###bump of unzip version for GCC 15 etc
+#wget -nc "https://marc.info/?l=buildroot&m=175725752612610&q=raw" -O "$CONFIG_DIR/../patches/buildroot_unzip_cmake.patch" 2>/dev/null
+
+##patches I'm working on getting into buildroot project, or someone else already has submitted but hasn't made a release yet
+for x in "$CONFIG_DIR"/../patches/buildroot_*.patch
 do
-  patch -N -p1 < "$CONFIG_DIR/../patches/$x.patch"
+  echo "applying $x"
+  patch -N -p1 < "$x"
 done
 
 ##add to config somewhat manually
